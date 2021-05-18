2b7ff93395220ce1721e37a84e9848c9ff384dbd
i.orexin <nutsmany@mail.ru>
2021-05-18 21:09:03 +0500

    Textures and pointing (not working yet)

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,13 +5,13 @@ public class CameraController : MonoBehaviour
     public Transform player;
     public float smoothness = 0.1f;
 
-    private Vector2 _velocity = Vector2.zero;
+    private Vector3 _velocity = Vector3.zero;
 
     void Update()
     {
         var playerPos = player.position;
-        var cameraPos = new Vector2 {x = playerPos.x, y = playerPos.y};
+        var cameraPos = new Vector3 {x = playerPos.x, y = playerPos.y, z = playerPos.z - 10};
 
-        transform.position = Vector2.SmoothDamp(transform.position, cameraPos, ref _velocity, smoothness);
+        transform.position = Vector3.SmoothDamp(transform.position, cameraPos, ref _velocity, smoothness);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,14 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     public Rigidbody2D rb;
+    public Camera cam;
 
     private Vector2 _moveDirection;
+    private Vector2 _mousePos;
     
     void Update()
     {
@@ -23,10 +26,16 @@ public class PlayerController : MonoBehaviour
         var moveY = Input.GetAxisRaw("Vertical");
 
         _moveDirection = new Vector2(moveX, moveY).normalized;
+
+        _mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     void Move()
     {
-        rb.velocity = new Vector2(_moveDirection.x * moveSpeed, _moveDirection.y * moveSpeed);
+        rb.MovePosition(rb.position + _moveDirection * moveSpeed * Time.fixedDeltaTime);
+        
+        var lookDirection = _mousePos - rb.position;
+        var mouseAngle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = mouseAngle;
     }
 }
